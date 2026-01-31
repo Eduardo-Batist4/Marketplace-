55a6dc6c2df981162d766fc74b56aaabbc80e6d5
Eduardo Batista <carloswe3st@gmail.com>
2026-01-30 22:29:38 -0300

    feat: adding ProductSummaryResource

---
+++ src/app/Http/Resources/ProductSummaryResource.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProductSummaryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'category_id' => $this->category_id,
+            'image_path' => $this->image_path,
+            'description' => $this->description,
+            'discounts' => DiscountResource::collection(
+                $this->whenLoaded('discounts')
+            ),
+        ];
+    }
+}
