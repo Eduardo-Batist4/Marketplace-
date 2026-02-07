d9e42ebe6f8527f29b5c2688a1411d818dbff2f3
Eduardo Batista <carloswe3st@gmail.com>
2026-02-07 07:10:30 -0300

    feat: Access Denied Exception created

--- src/app/Exceptions/AccessDeniedException.php
+++ src/app/Exceptions/AccessDeniedException.php
@@ -6,10 +6,25 @@ use Exception;
 
 class AccessDeniedException extends Exception
 {
-    public function render($request)
+    protected $action;
+
+    public function __construct($message = null, $action = null)
     {
-        return response()->json([
-            'error' => "Permission denied!"
-        ], 403);
+        $this->action = $action;
+        $message = $message ?? 'You do not have permission to perform this action';
+        parent::__construct($message);
+    }
+
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    public function context()
+    {
+        return [
+            'action' => $this->action,
+            'user_id' => auth()->id(),
+        ];
     }
 }
